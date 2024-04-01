263f20fd1c9bf1ea3d37119d14da9ab2d724139f
Mathieu Le Puil <lepuilmathieu@gmail.com>
2024-04-01 12:20:42 +0200

    feat: add authentication with gitlab

---
+++ src/Controller/GitlabController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use KnpU\OAuth2ClientBundle\Client\ClientRegistry;
+use League\OAuth2\Client\Provider\Exception\IdentityProviderException;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
+
+class GitlabController extends AbstractController
+{
+    #[Route('/login/gitlab', name: 'gitlab_connect', methods: ['GET'])]
+    public function gitlabConnect(ClientRegistry $clientRegistry): Response
+    {
+        $client = $clientRegistry->getClient('gitlab');
+        return $client->redirect(['read_user']);
+    }
+
+    #[Route('/login/gitlab/check', name: 'gitlab_connect_check', methods: ['GET'])]
+    public function gitlabConnectCheck(ClientRegistry $clientRegistry, EntityManagerInterface $em, TokenStorageInterface $tokenStorage, SessionInterface $session): Response
+    {
+        $client = $clientRegistry->getClient('gitlab');
+
+        try {
+            $user = $client->fetchUser();
+
+            $userData = $user->toArray();
+
+            $fullName = $userData['name'];
+            $nameParts = explode(' ', $fullName);
+
+            $firstName = array_shift($nameParts);
+            $lastName = implode(' ', $nameParts);
+
+            $userRepository = $em->getRepository(User::class);
+            $existingUser = $userRepository->findOneBy(['email' => $userData['email']]);
+
+            if($existingUser) {
+                $existingUser->setFirstname($firstName);
+                $existingUser->setLastname($lastName);
+                $newUser = $existingUser;
+            } else {
+                $newUser = new User();
+                $newUser->setUsername($userData['username']);
+                $newUser->setEmail($userData['email']);
+                $newUser->setOauth('gitlab');
+                $newUser->setRoles(['ROLE_USER']);
+                $newUser->setProfilePicture($userData['avatar_url']);
+                $newUser->setPassword('gitlab');
+                $newUser->setFirstname($firstName);
+                $newUser->setLastname($lastName);
+            }
+
+            $em->persist($newUser);
+            $em->flush();
+
+            $token = new UsernamePasswordToken($newUser, 'main', $newUser->getRoles());
+
+            $tokenStorage->setToken($token);
+            $session->set('_security_main', serialize($token));
+
+            return $this->redirectToRoute('app_home');
+
+        } catch (IdentityProviderException $e) {
+            var_dump($e->getMessage()); die;
+        }
+    }
+}
