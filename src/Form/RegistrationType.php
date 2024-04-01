b67c1fc52578a436b635387ab6f4985a3b608e2e
Mathieu Le Puil <lepuilmathieu@gmail.com>
2024-04-01 11:34:01 +0200

    feat: add authentication with email and password

---
+++ src/Form/RegistrationType.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class RegistrationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('email')
+            ->add('password', RepeatedType::class)
+            ->add('firstname')
+            ->add('lastname')
+            ->add('username')
+            ->add('profile_picture', FileType::class, [
+                'mapped' => false,
+                'required' => false,
+            ])
+            ->add('submit', SubmitType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
