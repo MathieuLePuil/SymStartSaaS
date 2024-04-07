e9ac06bf36071e15d114d70132a61e19cc80528d
Mathieu Le Puil <lepuilmathieu@gmail.com>
2024-04-07 18:01:52 +0200

    feat: modify welcomeMessage for instruction

--- scripts/PostInstall.php
+++ scripts/PostInstall.php
@@ -9,9 +9,9 @@
             $projectFolderName = basename(getcwd());
 
             echo "\nWelcome to SymStartSaaS!\n\n";
-            echo "> cd " . $projectFolderName . "\n";
-            echo "> composer install\n";
-            echo "> npm install\n\n";
+            echo "\t> cd " . $projectFolderName . "\n";
+            echo "\t> composer install\n";
+            echo "\t> npm install\n\n";
             echo "You can find the documentation at https://symstartsaas.mathieulp.fr/docs\n\n";
         }
     }
